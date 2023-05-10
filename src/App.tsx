36716f05ddfc9cc84000446ec2e34ceee49bdd9f
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2023-05-10 09:59:03 +0800

    feat: add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { Analytics } from '@vercel/analytics/react';
 import ChatContainer from 'containers/chat';
 import { initDB } from 'react-indexed-db';
 import { DBConfig } from 'store/db/config';
@@ -8,9 +9,12 @@ initDB(DBConfig);
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <ChatContainer />
-    </ChakraProvider>
+    <>
+      <Analytics />
+      <ChakraProvider theme={theme}>
+        <ChatContainer />
+      </ChakraProvider>
+    </>
   );
 }
 
