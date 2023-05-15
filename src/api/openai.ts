840a0b73fdf8ab476a7f6ce9d619f5cb35e56089
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2023-05-15 12:59:12 +0800

    feat: getUsage filter update

--- src/api/openai.ts
+++ src/api/openai.ts
@@ -8,6 +8,12 @@ const openaiAPI = axios.create({
   },
 });
 
+const getNextMonthFirstDate = () => {
+  const today = new Date();
+  const nextMonth = new Date(today.getFullYear(), today.getMonth() + 1);
+  return new Date(nextMonth.getFullYear(), nextMonth.getMonth(), 1);
+};
+
 export const getUsages = () => {
   return openaiAPI.get<{
     total_usage: number;
@@ -18,7 +24,7 @@ export const getUsages = () => {
   }>('/dashboard/billing/usage', {
     params: {
       start_date: format(new Date(), 'yyyy-MM-01'),
-      end_date: format(lastDayOfMonth(new Date()), 'yyyy-MM-dd'),
+      end_date: format(getNextMonthFirstDate(), 'yyyy-MM-dd'),
     },
   });
 };
