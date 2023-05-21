909f9e83871c140ef0630c748d95856e63bd618f
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2023-05-21 19:03:27 +0800

    chore: move message

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { Box, ChakraProvider, Flex, Link } from '@chakra-ui/react';
 import { Analytics } from '@vercel/analytics/react';
-import ChatContainer from 'containers/chat';
 import { initDB } from 'react-indexed-db';
 import { DBConfig } from 'store/db/config';
 import { theme } from 'theme';
@@ -12,7 +11,14 @@ function App() {
     <>
       <Analytics />
       <ChakraProvider theme={theme}>
-        <ChatContainer />
+        <Flex justify="center" align="center" h="100vh" w="full">
+          <Box>
+            We have moved to{' '}
+            <Link href="https://chat.lesseffort.io" as="span" color="blue.500">
+              lesseffort.io
+            </Link>
+          </Box>
+        </Flex>
       </ChakraProvider>
     </>
   );
