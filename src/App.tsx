0b6fd9e39494307f929c4dc3837bed45662d70af
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2023-05-24 01:06:51 +0800

    feat: add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { Analytics } from '@vercel/analytics/react';
 import ChatContainer from 'containers/chat';
+import ReactGA from 'react-ga4';
 import { initDB } from 'react-indexed-db';
 import { DBConfig } from 'store/db/config';
 import { theme } from 'theme';
+
+ReactGA.initialize('G-5RCYEPQFPF');
 
 initDB(DBConfig);
 
 function App() {
   return (
-    <>
-      <Analytics />
-      <ChakraProvider theme={theme}>
-        <ChatContainer />
-      </ChakraProvider>
-    </>
+    <ChakraProvider theme={theme}>
+      <ChatContainer />
+    </ChakraProvider>
   );
 }
 
