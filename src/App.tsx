65aad6573e3ee93aa80d0f18ad7283450a214a1a
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2023-05-24 04:36:37 +0800

    chore: sentry production only

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,7 @@ import { DBConfig } from 'store/db/config';
 import { theme } from 'theme';
 
 Sentry.init({
-  environment: 'production',
-  dsn: 'https://4041f547d61845bfb2242755892a5323@o4505234435276800.ingest.sentry.io/4505234436259840',
+  dsn: process.env.SENTRY_DSN,
   integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
   // Performance Monitoring
   tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
