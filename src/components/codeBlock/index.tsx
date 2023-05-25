31d6cec30ff70e8e913731f8386c3b0f384903db
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2023-05-25 19:15:03 +0800

    feat: space for actions on code block

--- src/components/codeBlock/index.tsx
+++ src/components/codeBlock/index.tsx
@@ -4,56 +4,62 @@ import { DownloadTextButton } from 'components/downloadTextButton';
 import { TbCopy, TbDownload } from 'react-icons/tb';
 import { CodeProps } from 'react-markdown/lib/ast-to-react';
 
-export const CodeBlock = memo(({ node, inline, ...props }: CodeProps) => {
-  const textInput = useRef<HTMLElement>(null);
-  const [lang, setLang] = useState('');
+export const CodeBlock = memo(
+  ({ node, inline, children, ...props }: CodeProps) => {
+    const codeBlockRef = useRef<HTMLElement>(null);
+    const [lang, setLang] = useState('');
 
-  useLayoutEffect(() => {
-    let className = props.className;
-    setLang(className?.replace('hljs language-', '') || '');
-  }, []);
+    useLayoutEffect(() => {
+      let className = props.className;
+      setLang(className?.replace('hljs language-', '') || '');
+    }, []);
 
-  const handleCopy = () => {
-    navigator.clipboard.writeText(textInput?.current?.textContent || '');
-  };
+    const handleCopy = () => {
+      navigator.clipboard.writeText(codeBlockRef?.current?.textContent || '');
+    };
 
-  if (inline) {
-    return <code {...props} />;
-  }
+    if (inline) {
+      return <code {...props} />;
+    }
 
-  return (
-    <Box pos="relative">
-      {!!lang && (
-        <Box fontSize="sm" color="gray.400" mb={4}>
-          {lang}
+    return (
+      <Box pos="relative">
+        {!!lang && (
+          <Box fontSize="sm" color="gray.400" mb={4}>
+            {lang}
+          </Box>
+        )}
+        <Box as="code" ref={codeBlockRef} {...props}>
+          {/* Add more width when have more actions */}
+          <Box w="5rem" h="2rem" float="right" />
+          {children}
         </Box>
-      )}
-      <Box as="code" ref={textInput} {...props} />
-      <HStack pos="absolute" top={-2} right={-2}>
-        <DownloadTextButton
-          aria-label="Download text button"
-          icon={<TbDownload />}
-          text={textInput?.current?.textContent || ''}
-          lang={lang}
-          size="sm"
-          fontSize="md"
-          _light={{
-            color: 'white',
-            bgColor: 'whiteAlpha.400',
-          }}
-        />
-        <IconButton
-          icon={<TbCopy />}
-          aria-label="Copy code"
-          onClick={handleCopy}
-          size="sm"
-          fontSize="md"
-          _light={{
-            color: 'white',
-            bgColor: 'whiteAlpha.400',
-          }}
-        />
-      </HStack>
-    </Box>
-  );
-});
+        <HStack pos="absolute" top={-2} right={-2}>
+          <DownloadTextButton
+            aria-label="Download text button"
+            icon={<TbDownload />}
+            text={codeBlockRef?.current?.textContent || ''}
+            lang={lang}
+            size="sm"
+            fontSize="md"
+            _light={{
+              color: 'white',
+              bgColor: 'whiteAlpha.400',
+            }}
+          />
+          <IconButton
+            icon={<TbCopy />}
+            aria-label="Copy code"
+            onClick={handleCopy}
+            size="sm"
+            fontSize="md"
+            _light={{
+              color: 'white',
+              bgColor: 'whiteAlpha.400',
+            }}
+          />
+        </HStack>
+      </Box>
+    );
+  },
+);
