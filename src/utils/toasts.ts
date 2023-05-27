82b56b7ec88722ef4f3cd5ca3b02ee6070aae94a
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2023-05-28 00:55:59 +0800

    feat: limitation tracker

--- src/utils/toasts.ts
+++ src/utils/toasts.ts
@@ -1,4 +1,5 @@
 import { standaloneToast } from 'index';
+import ReactGA from 'react-ga4';
 
 export const toastForFreeUser = (
   id: string,
@@ -6,6 +7,10 @@ export const toastForFreeUser = (
   description?: string,
 ) => {
   if (!standaloneToast.isActive(id)) {
+    ReactGA.event({
+      action: `Limit ${id}`,
+      category: 'Action',
+    });
     standaloneToast({
       id,
       title,
