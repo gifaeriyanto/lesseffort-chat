a7dc645cc1aa6df6c35368ec85511e5832e62015
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2023-06-27 17:54:24 +0800

    feat: list of models order

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,12 +1,12 @@
 export enum OpenAIModel {
-  'gpt-4' = 'gpt-4',
-  'gpt-3.5-turbo-16k' = 'gpt-3.5-turbo-16k',
   'gpt-3.5-turbo' = 'gpt-3.5-turbo',
-  'gpt-4-0613' = 'gpt-4-0613',
-  'gpt-4-0314' = 'gpt-4-0314',
-  'gpt-3.5-turbo-16k-0613' = 'gpt-3.5-turbo-16k-0613',
   'gpt-3.5-turbo-0613' = 'gpt-3.5-turbo-0613',
   'gpt-3.5-turbo-0301' = 'gpt-3.5-turbo-0301',
+  'gpt-3.5-turbo-16k' = 'gpt-3.5-turbo-16k',
+  'gpt-3.5-turbo-16k-0613' = 'gpt-3.5-turbo-16k-0613',
+  'gpt-4' = 'gpt-4',
+  'gpt-4-0613' = 'gpt-4-0613',
+  'gpt-4-0314' = 'gpt-4-0314',
 }
 
 export const defaultModel = OpenAIModel['gpt-3.5-turbo-16k'];
