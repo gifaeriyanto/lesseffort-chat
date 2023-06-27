766e38c7a4fe41567e406bf4d6031174cc2a41e9
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2023-06-27 16:57:25 +0800

    fix: gpt 4 32k added back

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -3,7 +3,7 @@ export enum OpenAIModel {
   GPT_3_5_16K = 'gpt-3.5-turbo-16k',
   GPT_3_5_LEGACY = 'gpt-3.5-turbo-0301',
   GPT_4 = 'gpt-4-0613',
-  // GPT_4_32K = 'gpt-4-32k',
+  GPT_4_32K = 'gpt-4-32k-0314',
 }
 
 export const defaultBotInstruction = 'You are a helpful AI assistant.';
