7b91bca6264c3b96b77fe539baec7aa23e0c9fe0
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2023-06-27 16:54:59 +0800

    fix: gpt 4 32k removed

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -2,8 +2,8 @@ export enum OpenAIModel {
   GPT_3_5 = 'gpt-3.5-turbo-0613',
   GPT_3_5_16K = 'gpt-3.5-turbo-16k',
   GPT_3_5_LEGACY = 'gpt-3.5-turbo-0301',
-  GPT_4 = 'gpt-4-0631',
-  GPT_4_32K = 'gpt-4-32k',
+  GPT_4 = 'gpt-4-0613',
+  // GPT_4_32K = 'gpt-4-32k',
 }
 
 export const defaultBotInstruction = 'You are a helpful AI assistant.';
